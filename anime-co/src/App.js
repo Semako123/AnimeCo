98b11a760ff3f288f76c6438ee779bfa68c9912e
semako123 <zosusemenoch@gmail.com>
2022-09-10 14:40:43 +0100

    Integrate React Bootstrap for front-end Set up navigation of the application

--- anime-co/src/App.js
+++ anime-co/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
+import ResponsiveDrawer from './components/drawer/drawer';
+import {ThemeProvider, createTheme} from '@mui/material/styles'
+import { CssBaseline } from '@mui/material'
+
+const dark_theme = createTheme(
+  {
+    palette:{mode:'dark',}
+  },
+)
+
 
 function App() {
   return (
     <React.Fragment>
-      
+      <ThemeProvider theme={dark_theme}>
+    <CssBaseline/>
+      <ResponsiveDrawer/>
+      </ThemeProvider>
     </React.Fragment>
   );
 }
